782de42bbcdb1eeceb5c7c64c750dc31886c8637
Artur Kondas <devilyoushy@gmail.com>
2021-06-08 11:34:54 +0100

    Update Rust bits

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,61 +1,80 @@
-use std::time::{SystemTime, UNIX_EPOCH};
+use bytes::Bytes;
 use futures::future::ok;
 use futures::stream::once;
-use bytes::{Bytes};
+use std::time::{SystemTime, UNIX_EPOCH};
+use uuid::Uuid;
 
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
-struct File {
-    name: String,
-    time: u64,
-    err: String
-}
-
-#[derive(Deserialize)]
-struct Download {
-    name: String,
+#[derive(Serialize, Deserialize)] // why do I have to do that?
+struct Tweet {
+    id: Uuid,
+    author: String,
+    body: String,
+    likes: i64,
 }
 
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello, Sunshine!")
 }
 
-async fn upload() -> impl Responder {
-    println!("Uploading...");
-    let u = &File {
-        name: "dummy data".to_string(),
-        time: SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs(),
-        err: "".to_string()
-    };
-
-    HttpResponse::Ok().json(u)
+async fn get_tweets() -> impl Responder {
+    // Mock response
+    HttpResponse::NotImplemented().body("get_tweets not implemented")
 }
 
-async fn download(info: web::Path<Download>) -> HttpResponse {
-    println!("Downloading...");
-    let name = String::from(info.name.as_str());
-    let body = once(ok::<_, actix_web::Error>(Bytes::from(name)));
+async fn post_tweet(author: String, body: String) -> impl Responder {
+    let _t = &Tweet {
+        id: Uuid::new_v4(),
+        author,
+        body,
+        likes: 0,
+    }; // how to read body response?
 
-    HttpResponse::Ok()
-        .content_type("application/json")
-        .streaming(body)
+    HttpResponse::NotImplemented().body("post_tweet not implemented")
+}
+
+async fn get_single_tweet() -> impl Responder {
+    // Mock response
+    HttpResponse::NotImplemented().body("get_single_tweet not implemented")
+}
+
+async fn delete_single_tweet() -> impl Responder {
+    // Mock response
+    HttpResponse::NotImplemented().body("delete_single_tweet not implemented")
+}
+
+async fn get_tweet_likes() -> impl Responder {
+    // Mock response
+    HttpResponse::NotImplemented().body("get_tweet_likes not implemented")
+}
+
+async fn post_tweet_like() -> impl Responder {
+    // Mock response
+    HttpResponse::NotImplemented().body("post_tweet_like not implemented")
+}
+
+async fn delete_tweet_like() -> impl Responder {
+    // Mock response
+    HttpResponse::NotImplemented().body("delete_tweet_like not implemented")
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(index))
-            .service(
-                web::scope("/api")
-                .route("/files/", web::post().to(upload))
-                .route("/files/{name}/", web::get().to(download))
-            )
+        App::new().route("/", web::get().to(index)).service(
+            web::scope("/api")
+                .route("/tweets/", web::get().to(get_tweets))
+                .route("/tweets/", web::post().to(post_tweet))
+                .route("/tweets/{id}/", web::get().to(get_single_tweet))
+                .route("/tweets/{id}/", web::delete().to(delete_single_tweet))
+                .route("/tweets/{id}/likes/", web::get().to(get_tweet_likes))
+                .route("/tweets/{id}/likes/", web::post().to(post_tweet_like))
+                .route("/tweets/{id}/likes/", web::delete().to(delete_tweet_like)),
+        )
     })
     .bind("127.0.0.1:9000")?
     .run()
     .await
 }
-
